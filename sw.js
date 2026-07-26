2e53f56c778542c30f03280d35cf4322b3cf5269
linariteblue <linalightblue@gmail.com>
2026-07-26 22:15:48 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,26 @@
+// 一度読み込んだら、あとは機内でも動くようにまるごと保管しておく
+const CACHE = "nejinuki-v1";
+const FILES = ["./", "./index.html", "./manifest.webmanifest",
+               "./icon-180.png", "./icon-192.png", "./icon-512.png"];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
+});
+self.addEventListener("activate", e => {
+  e.waitUntil(caches.keys()
+    .then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k))))
+    .then(() => self.clients.claim()));
+});
+// 保管してあるものを最優先で返す。無いものだけ取りにいく
+self.addEventListener("fetch", e => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(
+    caches.match(e.request, { ignoreSearch: true }).then(hit =>
+      hit || fetch(e.request).then(res => {
+        const copy = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy)).catch(() => {});
+        return res;
+      }).catch(() => caches.match("./index.html"))
+    )
+  );
+});
